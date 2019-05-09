f12f4ab9bbe7b19d9077634df226bbb5ee218d4f
ninathe <nina.hermanrud@gmail.com>
2019-05-09 13:46:30 +0200

    dataset

---
+++ Dataset/9.Nov/Annotationss.py
@@ -0,0 +1,26 @@
+import json
+file = open("annotations.txt", "w")
+annotation_num = 0
+
+with open('annotations.json') as f:
+    data = json.load(f)
+
+for image in data["images"]:
+    file.write(image["file_name"])
+    file.write(' ') 
+
+    for annotation in data["annotations"]:
+        if(image["id"] == annotation["image_id"]):
+            annotation_num += 1
+            bbox = annotation["bbox"]
+            xmin = bbox[0]
+            ymin = bbox[3]
+            xmax = bbox[1]
+            ymax = bbox[2]
+            category = annotation["category_id"]
+            file.write("%s,%s,%s,%s,%s" % (xmin, ymin, xmax, ymax, category))
+            file.write(' ') 
+file.write("Annotations: " + str(annotation_num))
+         
+
+file.close()
